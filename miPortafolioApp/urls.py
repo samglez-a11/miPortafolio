48fbee95294caa74a20dff7fbbdb9acdfc85eb5f
samglez <samglez852@gmail.com>
2023-03-22 13:19:49 -0600

    Actualizacion

--- miPortafolioApp/urls.py
+++ miPortafolioApp/urls.py
@@ -4,7 +4,7 @@ from miPortafolioApp import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('trabajos', views.trabajos, name="trabajos"),
+    path('trabajos', views.trabajos, name="works"),
     path('about', views.about, name="about"),
     path('hobby', views.hobby, name="hobby")
 ]
